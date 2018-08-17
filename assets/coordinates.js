c86337861a4d0ef0833804d12385d9d31fc843a3
paskuale <pellicanipasquale@gmail.com>
2018-08-18 00:53:05 +0200

    js code updated for geocoding

--- assets/coordinates.js
+++ assets/coordinates.js
@@ -59,7 +59,33 @@ alexantr.coordinatesWidget = (function (d) {
             center: [opt.lat, opt.lng],
             zoom: opt.zoom,
             controls: ['smallMapDefaultSet']
+        }),
+        // Creating an instance of the ymaps.control.SearchControl class.
+            mySearchControl = new ymaps.control.SearchControl({
+            options: {
+                noPlacemark: true
+            }
+        }),
+            // The search results will be placed in the collection.
+            mySearchResults = new ymaps.GeoObjectCollection(null, {
+                hintContentLayout: ymaps.templateLayoutFactory.createClass('$[properties.name]')
+            });
+        myMap.controls.add(mySearchControl);
+        myMap.geoObjects.add(mySearchResults);
+        // When the found object is clicked, the placemark turns red.
+        mySearchResults.events.add('click', function (e) {
+            e.get('target').options.set('preset', 'islands#redIcon');
         });
+        // Putting the selected result in the collection.
+        mySearchControl.events.add('resultselect', function (e) {
+            var index = e.get('index');
+            mySearchControl.getResult(index).then(function (res) {
+                mySearchResults.add(res);
+            });
+        }).add('submit', function () {
+            mySearchResults.removeAll();
+        }),
+
         var marker;
         if (opt.showMarker) {
             marker = new ymaps.Placemark([opt.lat, opt.lng], {}, {preset: placemarkPreset});
