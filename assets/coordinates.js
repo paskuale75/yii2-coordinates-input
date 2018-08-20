76f7daa09246c86af8c27669a4080aea756065c3
paskuale <pellicanipasquale@gmail.com>
2018-08-20 22:53:38 +0200

    marker on model attribute

--- assets/coordinates.js
+++ assets/coordinates.js
@@ -92,6 +92,16 @@ alexantr.coordinatesWidget = (function (d) {
         // When the found object is clicked, the placemark turns red.
         mySearchResults.events.add('click', function (e) {
             e.get('target').options.set('preset', 'islands#redIcon');
+
+            // gestione del click e della scrittura delle coordinate
+            var coords = e.get('coords');
+            if (typeof marker !== 'undefined') {
+                yMap.geoObjects.remove(marker);
+            }
+            marker = new ymaps.Placemark(coords, {}, {preset: placemarkPreset});
+            yMap.geoObjects.add(marker);
+            changeInputValue(input, coords[0], coords[1]);
+
         });
         // Putting the selected result in the collection.
         mySearchControl.events.add('resultselect', function (e) {
